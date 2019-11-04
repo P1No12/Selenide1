3bb9b14ccf8e1e3aa48c1414bcb17322a4b849f5
P1No12 <mwww-go@yandex.ru>
2019-11-05 00:04:50 +0400

    222

---
+++ src/main/java/ru/test/SomeTest.java
@@ -0,0 +1,23 @@
+package ru.test;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.Selectors.*;
+
+public class SomeTest {
+    @Test
+    void shouldSearch() {
+        //cccgit commit -m "first commit"
+        open("http://google.com/ncr");
+        element(byName("q")).setValue("selenide").pressEnter();
+        elements("#search .g").shouldHave(sizeGreaterThanOrEqual(6)).first().shouldHave(text("Selenide: concise UI tests in Java"))
+                .find(".r>a").click();
+
+    }
+
+
+}
+
+
